add9f55d47b51d2222b746a68d637521652f82fb
Jimy Mora <jimymora@gmail.com>
2023-10-13 17:49:08 -0500

    como limpiar la consola

--- limpieza.py
+++ limpieza.py
@@ -2,6 +2,14 @@ from os import system
 
 nombre = input("Nombre: \n")
 edad = input("Dime edad: \n")
-# system('cls')
+system('cls')# va aqui para limpiar la consola y dejar solo el resultado del print
 print(f"tu nombre es {nombre} y tu edad es {edad}")
-system('cls')
+
+
+""" from os import system
+
+nombre = input("Nombre: \n")
+edad = input("Dime edad: \n")
+system('cls')# va aqui para limpiar la consola y dejar solo el resultado del print
+print(f"tu nombre es {nombre} y tu edad es {edad}")
+ """
