1be7cdbf6835a8a71ec1863b1e408ca8dea2c040
Jimy Mora <jimymora@gmail.com>
2023-10-14 10:34:13 -0500

    codigo funcionando para limpiar la salidad del codigo

--- funcion_limpieza.py
+++ funcion_limpieza.py
@@ -1,10 +1,11 @@
 import os
+#cdigo para ejecutar en forma limpia la salida del codigo en consola
 
 def limpiar_consola():
     if os.name == 'nt':  # Windows
         os.system('cls')
-    else:  # Unix/Linux
-        os.system('clear')
+    # else:  # Unix/Linux
+    #     os.system('clear')
 
 # Tu código aquí...
 nombre = input("Nombre: \n")
@@ -12,3 +13,4 @@ edad = input("Dime edad: \n")
 
 # Llama a la función para limpiar la consola
 limpiar_consola()
+print(f"tu nombre es {nombre} y tu edad son {edad} años")
